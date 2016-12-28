9a08e521da8118ce2e29fe5afd4edb6ee41cc9c3
TheCjw <thecjw@live.com>
2016-12-28 10:12:27 +0800

    Use ida 6.95 as default.

--- profile.ps1
+++ profile.ps1
@@ -16,7 +16,7 @@ function InitializePath {
     # adb.exe
     "$TOOLS_BASE_PATH/android/sdk/platform-tools/",
     # aapt.exe etc.
-    "$TOOLS_BASE_PATH/android/sdk/build-tools/25.0.0/",
+    "$TOOLS_BASE_PATH/android/sdk/build-tools/25.0.2/",
     # ndk-build
     $NDK_ROOT,
     # arm*readelf.exe etc.
