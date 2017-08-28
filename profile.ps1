17579c62ce2ce836b4ee2e1213ca519cc1456410
TheCjw <thecjw@live.com>
2017-08-28 11:05:38 +0800

    Update cmake path

--- profile.ps1
+++ profile.ps1
@@ -25,7 +25,7 @@ function InitializePath {
     # ndk-build
     "$env:ANDROID_NDK_HOME",
     # CMake
-    "$LOCAL_PROGRAMS/cmake/cmake-3.9.0/bin/",
+    "$LOCAL_PROGRAMS/cmake/cmake-3.9.1/bin/",
     # Pandoc
     "$env:LOCALAPPDATA/Pandoc/"
   )
